b4fe5db2b57d137c94e02d7ad7149b8b20ebadb0
Mike McGrath <mike.mcgrath@rallysportdirect.com>
2020-01-06 10:02:53 -0700

    re implement make method

--- src/Models/QueueTask.php
+++ src/Models/QueueTask.php
@@ -7,4 +7,20 @@ use Popcorn\Beans\Models\Model;
 class QueueTask extends Model
 {
 	protected $collectionName = 'queue.tasks';
+
+	public static function make($task, array $parameters, array $data)
+	{
+		return [
+			'created' => self::getDate('now'),
+			'title' => $task,
+			'parameters' => $parameters,
+			'when' => time(),
+			'priority' => isset($data['priority']) ? $data['priority'] : null,
+			'batch' => $data['batch'],
+			'locked_by' => null,
+			'locked_at' => null,
+			'sales_channel' => $data['sales_channel'],
+			'options' => $data,
+		];
+	}
 }
